628224accc1ad99e8c635cbe0536d0555d36d05c
ErickGiffoni <giffoni.erick@gmail.com>
2022-07-21 12:46:21 -0300

    init

---
+++ src/distributed/distributed.py
@@ -0,0 +1,8 @@
+# from utils.utils import loadConfig
+# from Utils.utils import loadConfig
+# from ..Utils.utils import loadConfig
+from utils.utils import loadConfig
+
+config = loadConfig()
+
+print(config)
