4a111fadc9e1b20ef15fd1b638eb25af27d94bb3
ErickGiffoni <giffoni.erick@gmail.com>
2022-07-26 12:58:03 -0300

    basic socket structure

--- src/central/central.py
+++ src/central/central.py
@@ -1,2 +1,38 @@
-print(f"Name: {__name__}")
-print(f"Package: {__package__}")
+import socket
+
+from utils.utils import loadConfig
+from utils.Comms import Comms
+
+class Central():
+   def __init__(self) -> None:
+      self.comms = Comms(isCentral=True)
+
+
+
+c = Central()
+c.comms.listenToDistributedServers()
+
+
+# config = loadConfig()
+
+# centralIp      = config["central"]["ip"]
+# distributedIp1 = config["distributed"]["1"]["ip"]
+# distributedIp2 = config["distributed"]["2"]["ip"]
+
+# centralPort      = config["central"]["port"]
+# distributedPort1 = config["distributed"]["1"]["port"]
+# distributedPort2 = config["distributed"]["2"]["port"]
+
+# ds1 = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+# # ds2 = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+# ds1.bind((centralIp, centralPort))
+# # ds2.bind((distributedIp2, centralPort))
+
+# ds1.listen(2)
+
+# while True:
+#    conn, addr = ds1.accept()
+#    print(f"[x] {addr} connected")
+#    conn.send("It worked\n".encode())
+#    conn.close()
--- src/distributed/distributed.py
+++ src/distributed/distributed.py
@@ -1,8 +1,31 @@
 # from utils.utils import loadConfig
 # from Utils.utils import loadConfig
 # from ..Utils.utils import loadConfig
+import socket
 from utils.utils import loadConfig
+
 
 config = loadConfig()
 
-print(config)
+centralIp      = config["central"]["ip"]
+distributedIp1 = config["distributed"]["1"]["ip"]
+distributedIp2 = config["distributed"]["2"]["ip"]
+
+centralPort      = config["central"]["port"]
+distributedPort1 = config["distributed"]["1"]["port"]
+distributedPort2 = config["distributed"]["2"]["port"]
+
+ds1 = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+ds2 = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+
+ds1.bind((distributedIp1, distributedPort1))
+ds2.bind((distributedIp2, distributedPort2))
+
+ds1.connect((centralIp, centralPort))
+ds2.connect((centralIp, centralPort))
+
+print(ds1.recv(1024).decode())
+print(ds2.recv(1024).decode())
+
+ds1.close()
+ds2.close()
