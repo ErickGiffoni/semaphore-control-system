843d385a51bbab6149fe6d864d8b105896698f7d
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-07-28 22:30:23 -0300

    Make distributed configuration generic

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -2,33 +2,23 @@ import json
 
 
 def loadConfig():
-   with open("utils/config.json") as configFile:
-      configData = json.load(configFile)
-   return configData
+    with open("utils/config.json") as configFile:
+        configData = json.load(configFile)
+    return configData
+
 
 def getCentralInfo():
-   config = loadConfig()
-   centralIp         = config["central"]["ip"]
-   centralPort       = config["central"]["port"]
-   return centralIp, centralPort
+    central = loadConfig()["central"]
+    return central["ip"], central["port"]
+
 
 def getDistributedInfo():
-   config = loadConfig()
-   distributedIp1    = config["distributed"]["1"]["ip"]
-   distributedPort1  = config["distributed"]["1"]["port"]
-   distributedIp2    = config["distributed"]["2"]["ip"]
-   distributedPort2  = config["distributed"]["2"]["port"]
+    return loadConfig()["distributed"]
 
-   return (distributedIp1, distributedPort1), (distributedIp2, distributedPort2)
 
 def whichDistributedIsAddr(addr):
-   # addr is a pair (hostaddr: str, port: int)
-   dst1, dst2 = getDistributedInfo()
-   if dst1[0] == addr[0] and dst1[1] == addr[1]:
-      return 1
-
-   elif dst2[0] == addr[0] and dst2[1] == addr[1]:
-      return 2
-
-   else:
-      return None
+    # addr is a pair (hostaddr: str, port: int)
+    for dist in getDistributedInfo():
+        if (dist["ip"], dist["port"]) == addr:
+            return dist["number"]
+    return None
