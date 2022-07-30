cdf48e7c31a56fcacf4e013248ba3f6ca03e6615
ErickGiffoni <giffoni.erick@gmail.com>
2022-07-30 18:41:49 -0300

    add led and timer config

--- src/utils/Config.py
+++ src/utils/Config.py
@@ -3,10 +3,10 @@ import json
 
 class Config:
     def __init__(self):
-        configData = self.loadConfig()
-        self.centralIP = configData["central"]["ip"]
-        self.centralPort = configData["central"]["port"]
-        self.distributedInfo = configData["distributed"]
+        self.configData = self.loadConfig()
+        self.centralIP = self.configData["central"]["ip"]
+        self.centralPort = self.configData["central"]["port"]
+        self.distributedInfo = self.configData["distributed"]
 
     def loadConfig(self):
         with open("utils/config.json") as configFile:
@@ -19,3 +19,17 @@ class Config:
             if (dist["ip"], dist["port"]) == addr:
                 return dist["number"]
         return None
+
+    def getRoadTimerInfo(self, isMainRoad=True):
+        return self.configData["timer"]["mainRoad" if isMainRoad else "auxRoad"]
+
+    def getDistributedJunction(self, distributedId: int, junctionId: int):
+        return self.distributedInfo[distributedId-1]["junction"][junctionId-1]
+
+    def getDistributedLeds(self, distributedId: int, junctionId:int, trafficId: int):
+        """
+        distributedId: distributed server ID - either 1 or 2
+        trafficId: traffic light ID - either 1 or 2
+        """
+        return self.getDistributedJunction(distributedId, junctionId)["leds"]\
+            [trafficId-1]
