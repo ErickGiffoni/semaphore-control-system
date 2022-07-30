29c3e9760b3df45bec192a16a27c1e5da036f955
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-07-30 15:26:50 -0300

    Add time control to TrafficLight
    + added function to change lights based on time;
    + new variables to manage time for each specific led;
    + new variable to check which light is on;
    + using perf_counter() because is more precise then time().

--- src/distributed/traffic_light/TrafficLight.py
+++ src/distributed/traffic_light/TrafficLight.py
@@ -1,24 +1,50 @@
 from threading import Thread
 from gpiozero import LED
-
+from time import perf_counter
 
 class TrafficLight(Thread):
-    def __init__(self, red_led, yellow_led, green_led):
+    def __init__(self, leds, timer):
         Thread.__init__(self)
-        self.green_light = LED(green_led)
-        self.red_light = LED(red_led)
-        self.yellow_light = LED(yellow_led)
+        self.green_light = LED(led["green"])
+        self.min_time_green_light = timer["green"]["min"]
+        self.max_time_green_light = timer["green"]["max"]
+
+        self.red_light = LED(led["red"])
+        self.min_time_red_light = timer["red"]["min"]
+        self.max_time_red_light = timer["red"]["max"]
+
+        self.yellow_light = LED(led["yellow"])
+        self.min_time_yellow_light = timer["yellow"]["min"]
+        self.max_time_yellow_light = timer["yellow"]["max"]
+
+        self.light_start_time = 0
+        self.current_light = ""
+
+    def change_lights():
+        if self.current_light == "red":
+            self.wait_and_then_change(self.min_time_red_light, self.turn_yellow_light_on)
+        if self.current_light == "yellow":
+            self.wait_and_then_change(self.min_time_yellow_light, self.turn_green_light_on)
+        if self.current_light == "green":
+            self.wait_and_then_change(self.min_time_green_light, self.turn_red_light_on)
 
     def turn_red_light_on(self):
-        self.__turn_light_on(self.red_light, self.yellow_light, self.green_light)
+        self.__turn_light_on("red", self.red_light, self.yellow_light, self.green_light)
 
     def turn_green_light_on(self):
-        self.__turn_light_on(self.green_light, self.yellow_light, self.red_light)
+        self.__turn_light_on("green", self.green_light, self.yellow_light, self.red_light)
 
     def turn_yellow_light_on(self):
-        self.__turn_light_on(self.yellow_light, self.red_light, self.green_light)
+        self.__turn_light_on("yellow", self.yellow_light, self.red_light, self.green_light)
 
-    def __turn_light_on(self, light_on, light_off1, light_off2):
-        light_on.on()
+    def __turn_light_on(self, light_name, light_on, light_off1, light_off2):
         light_off1.off()
         light_off2.off()
+        light_on.on()
+        self.light_start_time = perf_counter()
+        self.current_light = light_name
+
+    def wait_and_then_change(min_light_time, turn_light_on)
+        while perf_counter() < self.light_start_time + min_light_time:
+            pass
+        turn_light_on()
