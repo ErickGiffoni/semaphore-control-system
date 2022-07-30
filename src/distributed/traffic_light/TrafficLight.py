180fe3d3e23ae773c827680fd2e6f5ac1d25dba0
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-07-29 23:18:01 -0300

    TrafficLight: add missing self calls and remove unnecessary ones

--- src/distributed/traffic_light/TrafficLight.py
+++ src/distributed/traffic_light/TrafficLight.py
@@ -3,22 +3,22 @@ from gpiozero import LED
 
 
 class TrafficLight(Thread):
-    def __init__(self, green_led, red_led, yellow_led):
+    def __init__(self, red_led, yellow_led, green_led):
         Thread.__init__(self)
         self.green_light = LED(green_led)
         self.red_light = LED(red_led)
         self.yellow_light = LED(yellow_led)
 
-    def turn_red_light_on():
+    def turn_red_light_on(self):
         self.__turn_light_on(self.red_light, self.yellow_light, self.green_light)
 
-    def turn_green_light_on():
+    def turn_green_light_on(self):
         self.__turn_light_on(self.green_light, self.yellow_light, self.red_light)
 
-    def turn_yellow_light_on():
+    def turn_yellow_light_on(self):
         self.__turn_light_on(self.yellow_light, self.red_light, self.green_light)
 
-    def __turn_light_on(light_on, light_off1, light_off2):
-        self.light_on.on()
-        self.light_off1.off()
-        self.light_off2.off()
+    def __turn_light_on(self, light_on, light_off1, light_off2):
+        light_on.on()
+        light_off1.off()
+        light_off2.off()
