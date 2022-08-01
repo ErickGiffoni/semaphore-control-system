32ac387317e9f64feeb34ca9318df862ca688f5a
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-07-31 21:30:47 -0300

    Update Junction with new config

--- src/distributed/junction/Junction.py
+++ src/distributed/junction/Junction.py
@@ -1,25 +1,42 @@
+import json
 from threading import Thread
-
-from utils.Config import Config
 from distributed.buzzer.Buzzer import Buzzer
-from distributed.roads.AuxRoad import AuxRoad
-from distributed.roads.MainRoad import MainRoad
+from distributed.roads.Road import Road
 
 
 class Junction(Thread):
-   def __init__(self, junctionId: int, distributedServerId: int, config: Config):
-      Thread.__init__(self)
-      self.name = f"Junction number {junctionId} of distributed server {distributedServerId}"
-      self.config = config
-      self.mainRoad = MainRoad(distributedServerId, junctionId, self.config)
-      self.auxRoad  = AuxRoad(distributedServerId, junctionId, self.config)
-      self.buzzer   = Buzzer()
-      
-   def run(self):
-      print ("Starting " + self.name)
-      # do stuff
-      self.mainRoad.startMonitoring()
-      self.auxRoad.startMonitoring()
-      # exit
-      print("Exiting " + self.name)
-      return
+    def __init__(self, distributedServerId: int, junctionConfig: json):
+        Thread.__init__(self)
+        self.junctionId = junctionConfig["id"]
+        self.name = (
+            f"Junction number {self.junctionId} of distributed server {distributedServerId}"
+        )
+        mainRoadLights = []
+        auxRoadLights = []
+        for trafficLight in junctionConfig["trafficlights"]:
+            if trafficLight["road"] == "main":
+                mainRoadLights.append(trafficLight)
+            else:
+                auxRoadLights.append(trafficLight)
+        self.mainRoad = Road(
+            "main",
+            distributedServerId,
+            self.junctionId,
+            mainRoadLights
+        )
+        self.auxRoad = Road(
+            "aux",
+            distributedServerId,
+            self.junctionId,
+            auxRoadLights
+        )
+        self.buzzer = Buzzer()
+
+    def run(self):
+        print("Starting " + self.name)
+        self.mainRoad.start()
+        self.auxRoad.start()
+        self.mainRoad.join()
+        self.auxRoad.join()
+        print("Exiting " + self.name)
+        return
