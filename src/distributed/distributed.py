2b89d697353f17fe6e4ee2b35744a6ed68bd47f8
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-07-31 21:32:00 -0300

    Update Distributed with new config

--- src/distributed/distributed.py
+++ src/distributed/distributed.py
@@ -1,43 +1,36 @@
 from threading import Thread
-from time import sleep
 
 from utils.Comms import Comms
+from utils.Config import Config
 from .junction.Junction import Junction
 
 
 class Distributed(Thread):
-   def __init__(self, distributedId: int) -> None:
-      """
-      distributedId is either 1 or 2
-      """
-      Thread.__init__(self)
-      self.name = f"Distributed Server {distributedId}"
-      self.comms = Comms(isCentral=False, whichDistributed=distributedId)
-      self.junction1 = Junction(
-         junctionId=1, distributedServerId=distributedId, config=self.comms.config
-      )
-      self.junction2 = Junction(
-         junctionId=2, distributedServerId=distributedId, config=self.comms.config
-      )
+    def __init__(self, distributedId: int) -> None:
+        """
+        distributedId is either 1 or 2
+        """
+        Thread.__init__(self)
+        self.name = f"Distributed Server {distributedId}"
+        self.config = Config()
+        self.comms = Comms(False, distributedId, self.config)
+        self.junction = Junction(
+            distributedId,
+            self.config.getJunction(distributedId, 1)
+        )
+
+    def run(self):
+        print("Starting " + self.name)
+        self.junction.start()
+
+        print("Exiting " + self.name)
+        self.junction.join()
+
+        self.comms.closeMySocket()
+        return
 
 
-   def run(self):
-      print ("Starting " + self.name)
-      # start junctions
-      self.junction1.start()
-      self.junction2.start()
-
-      sleep(1)
-      print ("Exiting " + self.name)
-      # join junctions
-      self.junction1.join()
-      self.junction2.join()
-
-      # close socket
-      self.comms.closeMySocket()
-      return
-
-d  = Distributed(1)
+d = Distributed(1)
 d2 = Distributed(2)
 
 d.start()
