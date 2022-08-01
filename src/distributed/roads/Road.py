2a0d82a3bcd3f00a3fc19bee960a48a556a42992
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-07-31 22:13:48 -0300

    Make Road class use Thread

--- src/distributed/roads/Road.py
+++ src/distributed/roads/Road.py
@@ -1,9 +1,12 @@
-class Road:
+from threading import Thread
+
+class Road(Thread):
     def __init__(self, road, distributedServerId: int, junctionId: int, trafficLights: list):
+        Thread.__init__(self)
         self.road = road
         self.distributedServerId = distributedServerId
         self.junctionId = junctionId
         self.trafficLights = trafficLights
 
-    def startMonitoring(self):
+    def run(self):
         pass
