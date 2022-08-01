b25febc1284a41d6c433366a6ff742825c7c7150
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2022-07-31 21:19:59 -0300

    Update config.json structure
    + add real data from project pinout table
    + simplify json template

--- src/utils/Config.py
+++ src/utils/Config.py
@@ -3,33 +3,31 @@ import json
 
 class Config:
     def __init__(self):
-        self.configData = self.loadConfig()
-        self.centralIP = self.configData["central"]["ip"]
-        self.centralPort = self.configData["central"]["port"]
-        self.distributedInfo = self.configData["distributed"]
-
-    def loadConfig(self):
-        with open("utils/config.json") as configFile:
-            configData = json.load(configFile)
-        return configData
+        configData = self.__loadConfig()
+        self.centralIP = configData["central"]["ip"]
+        self.centralPort = configData["central"]["port"]
+        self.distributed = configData["distributed"]
+        self.timer = configData["timer"]
 
     def whichDistributedIsAddr(self, addr):
-        # addr is a pair (hostaddr: str, port: int)
-        for dist in self.distributedInfo:
+        """
+        addr is a pair (hostaddr: str, port: int)
+        """
+        for dist in self.distributed:
             if (dist["ip"], dist["port"]) == addr:
-                return dist["number"]
+                return dist["id"]
         return None
 
     def getRoadTimerInfo(self, isMainRoad=True):
-        return self.configData["timer"]["mainRoad" if isMainRoad else "auxRoad"]
+        return self.timer["mainRoad" if isMainRoad else "auxRoad"]
 
-    def getDistributedJunction(self, distributedId: int, junctionId: int):
-        return self.distributedInfo[distributedId-1]["junction"][junctionId-1]
+    def getJunction(self, distId: int, junctionId: int):
+        """
+        distId: distributed server ID - either 1 or 2
+        """
+        return self.distributed[distId - 1]["junction"][junctionId - 1]
 
-    def getDistributedLeds(self, distributedId: int, junctionId:int, trafficId: int):
-        """
-        distributedId: distributed server ID - either 1 or 2
-        trafficId: traffic light ID - either 1 or 2
-        """
-        return self.getDistributedJunction(distributedId, junctionId)["leds"]\
-            [trafficId-1]
+    def __loadConfig(self):
+        with open("utils/config.json") as configFile:
+            configData = json.load(configFile)
+        return configData
