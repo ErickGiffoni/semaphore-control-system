b8bad8209233bbca08d6d1088ae2ee9dfd536be9
Sergio de Almeida Cipriano Junior <sergiosacj@protonmail.com>
2022-08-04 00:17:51 -0300

    Use pthread_kill instead of raise_signal

--- src/distributed/sendSignal.py
+++ src/distributed/sendSignal.py
@@ -1,15 +1,18 @@
 import signal
 from time import sleep
-from threading import Thread
+import threading
 
 def send_signals():
     while True:
         sleep(2)
-        signal.raise_signal(signal.SIGUSR1)
+        signal.pthread_kill(threading.main_thread().ident, signal.SIGUSR1)
 
-sendSignal = Thread(target=send_signals, args=())
+sendSignal = threading.Thread(target=send_signals, args=())
 
 def send_messages_to_central_server(signum, frame):
     print("send message to central server!")
 
 signal.signal(signal.SIGUSR1, send_messages_to_central_server)
+
+sendSignal.start()
+sendSignal.join()
