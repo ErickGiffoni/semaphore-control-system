a5ebb8239959c4fed88ddb7b7521487f9e0e8005
ErickGiffoni <giffoni.erick@gmail.com>
2022-08-03 23:57:42 -0300

    add Sensor class

---
+++ src/distributed/Sensor.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+from threading import Thread
+
+from distributed.TrafficLight import TrafficLight
+
+
+class Sensor(Thread):
+   def __init__(self, pin, traffic_light: TrafficLight) -> None:
+      self.pin = pin
+      self.traffic_light = traffic_light
+      self.qtt_cars_crossed_red = 0
+      self.qtt_cars_total = 0
+
+      GPIO.setmode(GPIO.BCM)
+      GPIO.setup(self.pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+   def run(self):
+      while True:
+         input = GPIO.input(self.pin)
+         if input == True:
+            # passou carro
+            print("passou carro ai mano")
+            if self.traffic_light.current_light == "red":
+               self.qtt_cars_crossed_red += 1
+            
+            self.qtt_cars_total += 1
