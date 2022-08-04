41b9fe3e5c450b3791675cde7d25ba12b5b07fe4
Sergio de Almeida Cipriano Junior <sergiosacj@protonmail.com>
2022-08-03 23:06:17 -0300

    Manage pedestrian_button with events

--- src/distributed/distributed.py
+++ src/distributed/distributed.py
@@ -1,6 +1,6 @@
 import socket
 from sys import argv
-from threading import Thread
+from threading import Event
 
 from utils.Config import config
 from distributed.TrafficLight import TrafficLight
@@ -14,6 +14,8 @@ mainRoadLight = ""
 auxRoadLight = ""
 timer_main_road = config.getRoadTimerInfo()
 timer_aux_road = config.getRoadTimerInfo(False)
+event = Event()
+event2 = Event()
 
 for trafficLight in distributed["trafficlights"]:
     leds = {
@@ -23,9 +25,9 @@ for trafficLight in distributed["trafficlights"]:
     }
     pedestrian_button = trafficLight["pedestrian_button"]
     if trafficLight["road"] == "main":
-        mainRoadLight = TrafficLight(leds, timer_main_road, "", pedestrian_button)
+        mainRoadLight = TrafficLight(leds, timer_main_road, "", pedestrian_button, event, event2)
     else:
-        auxRoadLight = TrafficLight(leds, timer_aux_road, "red", pedestrian_button)
+        auxRoadLight = TrafficLight(leds, timer_aux_road, "red", pedestrian_button, event, event2)
 
 mainRoadLight.start()
 auxRoadLight.start()
