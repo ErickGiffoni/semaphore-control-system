142fc177e0dd2e37a64f9d5368a4051040fd4610
ErickGiffoni <giffoni.erick@gmail.com>
2022-08-04 00:37:53 -0300

    using Comms in central

--- src/central/central.py
+++ src/central/central.py
@@ -1,19 +1,25 @@
 import socket
 import sys
+
+from utils.Comms import Comms
 from utils.Config import config
 
 HOST = config.centralIP
 PORT = config.centralPort
 N_DISTRIBUTED = int(sys.argv[1])
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen(N_DISTRIBUTED)
-    while True:
-        conn, addr = s.accept()
-        with conn:
-            print('Connected by', addr)
-            while True:
-                data = conn.recv(1024)
-                if not data: break
-                conn.sendall(data)
+my_con = Comms(isCentral=True, whichDistributed=0, config=config)
+my_con.listenToDistributedServers()
+my_con.closeMySocket()
+
+# with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+#     s.bind((HOST, PORT))
+#     s.listen(N_DISTRIBUTED)
+#     while True:
+#         conn, addr = s.accept()
+#         with conn:
+#             print('Connected by', addr)
+#             while True:
+#                 data = conn.recv(1024)
+#                 if not data: break
+#                 conn.sendall(data)
